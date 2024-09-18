cafd02d561bbe8869fc9c633493a54e096f0efac
Pedro Souza <contato.pedromoc@gmail.com>
2024-09-18 04:49:40 -0300

    added repository of owner

--- algatransito-api/src/main/java/com/algaworks/algatransito/algatransito_api/controller/ProprietarioController.java
+++ algatransito-api/src/main/java/com/algaworks/algatransito/algatransito_api/controller/ProprietarioController.java
@@ -1,24 +1,27 @@
 package com.algaworks.algatransito.algatransito_api.controller;
 
 import com.algaworks.algatransito.algatransito_api.domain.model.Proprietario;
+import com.algaworks.algatransito.algatransito_api.domain.repository.ProprietarioRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 public class ProprietarioController {
 
-    @PersistenceContext
-    private EntityManager manager;
+    private final ProprietarioRepository proprietarioRepository;
 
     @GetMapping("/proprietarios")
     public List<Proprietario> listar() {
-        return manager.createQuery("from Proprietario", Proprietario.class).getResultList();
+        return proprietarioRepository.findAll();
     }
 
 }
